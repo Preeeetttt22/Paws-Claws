5014e4fbbf2459c19655cfb1f4920c7c44216a6b
Preeeetttt22 <preet220704@gmail.com>
2025-02-18 01:03:29 +0530

    first change

---
+++ app/src/main/java/com/mad/pawsclaws/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mad.pawsclaws
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/mad/pawsclaws/SignupActivity.kt
@@ -0,0 +1,4 @@
+package com.mad.pawsclaws
+
+class SignupActivity {
+}
